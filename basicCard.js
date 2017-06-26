01ff9155b4131bdded82e97c76212ca56d5de3bd
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-06-25 20:31:26 -0700

    asd

---
+++ basicCard.js
@@ -0,0 +1,10 @@
+var basic = function basicCard(front, back) {
+    this.front = front,
+        this.back = back
+    console.log("Front of Card: " + front);
+    console.log("Back of Card: " + back);
+};
+
+
+exports.basic = basic;
+
---
+++ clozeCard.js
@@ -0,0 +1,8 @@
+var cloze = function clozeCard(blank,partial,full){
+	this.blank = blank,
+	this.partial= partial,
+	this.full= full
+}
+
+exports.cloze = cloze;
+
