2222351d10219a886de6d4b9694ff81e508e1cf8
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-06-27 15:00:56 -0700

    asd

--- cardConstructor.js
+++ cardConstructor.js
@@ -1,13 +1,14 @@
-function Basic(front, back) {
+function Basic(front, back, image) {
     if (this instanceof Basic) {
         this.front = front,
             this.back = back,
+            this.image = image,
             console.log(front);
         console.log(back);
 
 
     } else {
-        return new Basic(front, back);
+        return new Basic(front, back, image);
     }
 }
 
